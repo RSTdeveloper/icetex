b3161268328eebd8ebb540065a99cb105ba44a18
unknown <toleko89@hotmail.com>
2023-08-25 16:16:43 -0500

    :star2: continuando con el maquetado del CSS para ser resposive

--- registro/admin.py
+++ registro/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import registro
 # Register your models here.
+
+class registroAdmin(admin.ModelAdmin):
+    readonly_fields= ('created','updated')
+
+admin.site.register(registro, registroAdmin)
--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class registro(models.Model):
+    nombre      =   models.CharField(max_length=50, blank=True, null=True)
+    apellido    =   models.CharField(max_length=50, blank=True, null=True)
+    documento   =   models.CharField(max_length=50, blank=True, null=True)
+    numCredito  =   models.CharField(max_length=50, blank=True, null=True)
+    created     =   models.DateTimeField(auto_now_add=True)
+    updated     =   models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name='Registro'
+        verbose_name_plural='Registros'
+
+    def __str__(self):
+        return self.documento
